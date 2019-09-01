cc55d7ca085ada56a9932792f09e525421209139
David <david.susskind@walkme.com>
2019-09-01 14:47:27 +0300

    final touch

--- src/DAL.js
+++ src/DAL.js
@@ -1,14 +1,14 @@
 const _ = require('lodash')
-const state = require('./state')
 
-function setByPath(path, value) {
+module.exports = state => {
+    function setByPath(path, value) {
     _.set(state, path, value)
-}
-function getByPath(path, defaultValue = {}) {
+    }
+    function getByPath(path, defaultValue = {}) {
     return _.get(state, path, defaultValue)
-}
-module.exports = {
+    }
+    return {
     setByPath,
     getByPath
+    }
 }
-
