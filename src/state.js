e0a9cd4deafce73dac7b8fc3c6d361ebed135168
David <david.susskind@walkme.com>
2019-09-01 21:15:54 +0300

    remove useless code

--- src/state.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    currentUser: { mail: 'eli.b@walkme.com', name: 'Eli Blitz' }// I'm giving here a hardcoded current user to simplify the examples, production would not look this way
-}
