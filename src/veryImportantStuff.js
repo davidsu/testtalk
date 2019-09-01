3d9fe9ada711f7296cff5070b42d6d88592e5b37
David <david.susskind@walkme.com>
2019-09-01 21:12:08 +0300

    commented chode to be changed, log red on exception

--- src/veryImportantStuff.js
+++ src/veryImportantStuff.js
@@ -1,4 +1,5 @@
 const _ = require('lodash')
+const chalk = require('chalk')
 module.exports = (userInfo, DAL) => {
     const veryImportantUsers = [
         { mail: 'eli.b@walkme.com', name: 'Eli Blitz' },
@@ -8,6 +9,7 @@ module.exports = (userInfo, DAL) => {
         const currentUser = userInfo.getCurrentUser()
         const isHeImportant = !!veryImportantUsers.find(user => _.isEqual(currentUser, user))
         if(!isHeImportant) {
+            console.error(chalk.red('is not important!!!'))
             throw 'current user is not important'
         }
         DAL.setByPath('importantUser', true)
