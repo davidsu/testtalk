e790d8ae8a0be5d854fa6aa93a3d7eae0137542f
David <david.susskind@walkme.com>
2019-09-01 11:57:21 +0300

    step2

--- __test__/veryImportantStuff.test.js
+++ __test__/veryImportantStuff.test.js
@@ -1,19 +1,19 @@
-const factory = require('../src/veryImportantStuff')
-let currentUser;
-let state = {}
-const mockUserInfo = {
-    getCurrentUser: () => currentUser,
-}
-const mockDAL = {
-    setByPath: (path, value) => state[path] = value,
-    getByPath: () => state
-}
+//lets mock only the application state
+//in a real world application this isn't feasible because many of the dependencies won't work on testTime and need mocking
+//our test doesn't have to know how to mock everything so the application will load
+const veryImportantFactory = require('../src/veryImportantStuff')
+const userInfoFactory = require('../src/userInfo')
+const DAL = require('../src/DAL')
+
+jest.mock('../src/state', () => ({
+    currentUser: { mail: 'eli.b@walkme.com', name: 'Eli Blitz' }
+}))
 describe('getCurrentUser', () => {
     beforeEach(() => state = {})
-    it('should return empty user when not loggedin', () => {
+    it('should set application state when current user is veryImportantFactory important', () => {
         currentUser = { mail: 'eli.b@walkme.com', name: 'Eli Blitz' }
-        const importantStuff = factory(mockUserInfo, mockDAL)
+        const importantStuff = veryImportantFactory(userInfoFactory(DAL), DAL)
         importantStuff.setVeryImportantUserState()
-        expect(state).toEqual({importantUser: true})
+        expect(DAL.getByPath('importantUser')).toBeTruthy()
     })
 })
