a217efe4b89bd1b9d3569659703cedf7f0c92d9a
David <david.susskind@walkme.com>
2019-09-02 00:59:19 +0300

    align improvements made in step3

--- src/userInfo.js
+++ src/userInfo.js
@@ -1,13 +1,13 @@
-
 export default function(DAL) {
     return {
         getCurrentUser: () => DAL.getByPath('currentUser'), //set default to null to fail test
         // getUser: () => DAL.getByPath('currentUser'), //set default to null to fail test
         setCurrentUser: user => {
-            const realMail = user.mail.replace(/\+.*@/g, '@')
+            const realMail = user.mail
+                                .replace(/\+.*@/g, '@')
             .replace(/\.(.*)@/g, '$1@')
-            // DAL.setByPath('currentUser', {realMail, ...user})
             DAL.setByPath('currentUser', user)
+            // DAL.setByPath('currentUser', {realMail, ...user})
         }
     }
 }
