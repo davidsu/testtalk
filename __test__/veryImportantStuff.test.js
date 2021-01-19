250f537e2babdc4b738a50150fb41c510f5d44de
David <david.susskind@walkme.com>
2021-01-19 21:05:07 +0200

    refactor: clean code

--- __test__/veryImportantStuff.test.js
+++ __test__/veryImportantStuff.test.js
@@ -1,23 +1,20 @@
 //lets mock only the application state
 //in a real world application this isn't feasible because many of the dependencies won't work on testTime and need mocking
 //our test doesn't have to know how to mock everything so the application will load
-import veryImportantFactory from '../src/veryImportantStuff'
-import userInfoFactory from '../src/userInfo'
-import DALfactory from '../src/DAL'
-
+import { setVeryImportantUserState } from '../src/veryImportantStuff'
+import DAL from '../src/DAL'
+import * as userInfo from '../src/userInfo'
 describe('getCurrentUser', () => {
     it('should set application state when current user is veryImportant', () => {
         const currentUser = { mail: 'eli.b@walkme.com', name: 'Eli Blitz' }
-        const DAL = DALfactory({currentUser})
-        const importantStuff = veryImportantFactory(userInfoFactory(DAL), DAL)
-        importantStuff.setVeryImportantUserState()
+        DAL.resetState({currentUser})
+        setVeryImportantUserState()
         expect(DAL.getByPath('importantUser')).toBeTruthy()
     })
 
     it('should throw for user that is not very important', () => {
         const currentUser = { mail: 'dummy@walkme.com', name: 'dummy' }
-        const DAL = DALfactory({currentUser})
-        const importantStuff = veryImportantFactory(userInfoFactory(DAL), DAL)
-        expect(importantStuff.setVeryImportantUserState).toThrow('dummy is not important')
+        DAL.resetState({currentUser})
+        expect(setVeryImportantUserState).toThrow('dummy is not important')
     })
 })
--- runSystem.js
+++ runSystem.js
@@ -1,10 +1,16 @@
 import app from './src/app.js'
 
-const {DAL, userInfo, veryImportantStuff} = app()
+const {getByPath, userInfo, veryImportantStuff, setVeryImportantUserState} = app()
 
 userInfo.setCurrentUser({ mail: 'eli.b@walkme.com', name: 'Eli Blitz' })
-veryImportantStuff.setVeryImportantUserState()
-console.log(JSON.stringify({
-    currentUser: DAL.getByPath('currentUser'),
-    importantUser: DAL.getByPath('importantUser')
-}, null, 4))
+try {
+    setVeryImportantUserState()
+    console.log(JSON.stringify({
+        currentUser: getByPath('currentUser'),
+        importantUser: getByPath('importantUser')
+    }, null, 4))
+} catch(ERROR) {
+    console.log('#'.repeat(30))
+    console.log({ERROR})
+    console.log('#'.repeat(30))
+}
