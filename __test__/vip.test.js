33f329520ff27d99e014de743962f8ffa9ce4355
David Susskind <david.susskind@walkme.com>
2021-08-30 15:59:22 +0300

    better naming

--- __test__/vip.test.js
+++ __test__/vip.test.js
@@ -1,7 +1,7 @@
 //lets mock only the application state
 //in a real world application this isn't feasible because many of the dependencies won't work on testTime and need mocking
 //our test doesn't have to know how to mock everything so the application will load
-import veryImportantFactory from '../src/veryImportantStuff'
+import vipFactory from '../src/vip'
 import userInfoFactory from '../src/userInfo'
 import DALfactory from '../src/DAL'
 
@@ -9,15 +9,15 @@ describe('getCurrentUser', () => {
     it('should set application state when current user is veryImportant', () => {
         const currentUser = { mail: 'eli.b@walkme.com', name: 'Eli Blitz' }
         const DAL = DALfactory({currentUser})
-        const importantStuff = veryImportantFactory(userInfoFactory(DAL), DAL)
-        importantStuff.setVeryImportantUserState()
+        const vip = vipFactory(userInfoFactory(DAL), DAL)
+        vip.set()
         expect(DAL.getByPath('importantUser')).toBeTruthy()
     })
 
     it('should throw for user that is not very important', () => {
         const currentUser = { mail: 'dummy@walkme.com', name: 'dummy' }
         const DAL = DALfactory({currentUser})
-        const importantStuff = veryImportantFactory(userInfoFactory(DAL), DAL)
-        expect(importantStuff.setVeryImportantUserState).toThrow('dummy is not important')
+        const vip = vipFactory(userInfoFactory(DAL), DAL)
+        expect(vip.set).toThrow('dummy is not important')
     })
 })
