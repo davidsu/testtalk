9a25bd5f8a7d14c59f2169749afbbed43dede9db
David Susskind <david.susskind@walkme.com>
2021-08-30 15:32:05 +0300

    rely on snippet to insert code

--- src/userInfo.js
+++ src/userInfo.js
@@ -1,12 +1,6 @@
 export default function(DAL) {
     return {
         getCurrentUser: () => DAL.getByPath('currentUser'), 
-        setCurrentUser: user => {
-            DAL.setByPath('currentUser', user)
-            // const realMail = user.mail
-            //                     .replace(/\+.*@/g, '@')
-            //                     .replace(/\.(.*)@/g, '$1@')
-            // DAL.setByPath('currentUser', {realMail, ...user})
-        }
+        setCurrentUser: user => DAL.setByPath('currentUser', user)
     }
 }
