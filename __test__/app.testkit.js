19aac9cefd4fe4714b85a5277300842e8f2495a9
David Susskind <david.susskind@walkme.com>
2021-09-01 18:32:28 +0300

    a little bit cleaner

--- __test__/app.testkit.js
+++ __test__/app.testkit.js
@@ -1,26 +1,30 @@
 import _ from 'lodash'
-import userInfoTKFactory from './userInfo.testkit'
 import DALTKFactory from './DAL.testkit'
 import vipTestkitFactory from './vip.testkit'
+import userInfoTKFactory from './userInfo.testkit'
 import app from '../src/app'
 
-export default () => {
-  const testKit = {}
-  const dependencies = {
-    userInfoTestKit: userInfoTKFactory(testKit),
-    DALtestKit: DALTKFactory(testKit),
-    vipTestkit: vipTestkitFactory(testKit)
-  }
+const combineTestkits = (allTestkits, testkit) => {
   const additions = {}
-  for(const kit of Object.values(dependencies)) {
+  for(const kit of Object.values(allTestkits)) {
     _.forOwn(kit, (val, key) => {
       if(typeof val === 'function') {
         additions[key] = (...args) => {
           val(...args)
-          return testKit
+          return testkit
         }
       }
     })
+  }
+  return additions
+}
+
+export default () => {
+  const testkit = {}
+  const dependencies = {
+    DALtestKit: DALTKFactory(testkit),
+    userInfoTestKit: userInfoTKFactory(testkit),
+    vipTestkit: vipTestkitFactory(testkit)
   }
 
   const build = () => {
@@ -31,6 +35,6 @@ export default () => {
     }
     return app(data)
   }
-  Object.assign(testKit, additions, dependencies, {build})
-  return testKit
+  Object.assign(testkit, combineTestkits(dependencies, testkit), dependencies, {build})
+  return testkit
 }
